7ae947d0140a37a82ab31670f02eb5f2c47a49fa
buqqq <154301335+buqqq@users.noreply.github.com>
2023-12-21 12:10:18 -0600

    test

---
+++ Claims-Main/src/buq/commands/ClaimCommand.php
@@ -0,0 +1,15 @@
+<?php
+
+namespace buq\commands;
+
+use pocketmine\player\Player;
+use pocketmine\command\Command;
+use pocketmine\command\CommandSender;
+
+class ClaimCommand extends Command {
+
+    public function __construct() {
+        parent::
+    }
+}
+?>
